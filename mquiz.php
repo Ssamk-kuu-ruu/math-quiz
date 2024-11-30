80b584989e74529596f6f5f87f194604f24d54ac
Ssamk-kuu-ruu <142188212+Ssamk-kuu-ruu@users.noreply.github.com>
2024-12-01 03:21:29 +0800

    connecting to web

--- mquiz.php
+++ mquiz.php
@@ -1,4 +1,16 @@
 <?php
+
+$host = "localhost";
+$user = "root";
+$pass = "";
+$dbname = "quizapp";
+$port = "3307";
+
+$conn = mysqli_connect($host, $user, $pass, $dbname, $port);
+
+if ($conn -> connect_error) {
+    die("Connection failed: " . $conn -> connect_error)
+}
 session_start();
 
 if (!isset($_SESSION['settings'])) {
